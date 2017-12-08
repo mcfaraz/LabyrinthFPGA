4a2120a878f2700aaabb5a15fb566d09387571d8
VigSyamasundar <VigSyamasundar@users.noreply.github.com>
2017-12-08 04:32:01 +0000

    Faraz SevenSeg

---
+++ Lab4 - VGA Output.srcs/sources_1/new/RGB_LED.v
@@ -0,0 +1,56 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 08.12.2017 04:06:08
+// Design Name: 
+// Module Name: RGB_LED
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module RGB_LED(
+    input clk,
+//    input LED16_R_IN,
+//    input LED16_G_IN,
+//    input LED16_B_IN,
+//    input LED17_R_IN,
+//    input LED17_G_IN,
+//    input LED17_B_IN,
+    output LED16_R,
+    output LED16_G,
+    output LED16_B
+//    output LED17_R,
+//    output LED17_G,
+//    output LED17_B
+    );
+
+always @ (posedge clk)
+begin
+    
+end
+
+
+
+  
+endmodule
+
+
+
+
+
+
+
+
+
